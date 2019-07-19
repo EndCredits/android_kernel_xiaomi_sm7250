cadde0c2df897d4a67a93c0ca0d199ed55af78f3
freak07 <michalechner92@googlemail.com>
2019-07-19 10:45:25 +0200

    techpack: use more power efficient workingqueues

--- techpack/audio/asoc/codecs/wsa881x-analog.c
+++ techpack/audio/asoc/codecs/wsa881x-analog.c
@@ -926,7 +926,7 @@ static void wsa881x_ocp_ctl_work(struct work_struct *work)
 		snd_soc_component_update_bits(component,
 					WSA881X_SPKR_OCP_CTL, 0xC0, 0xC0);
 
-	schedule_delayed_work(&wsa881x->ocp_ctl_work,
+ 	queue_delayed_work(system_power_efficient_wq, &wsa881x->ocp_ctl_work,
 			msecs_to_jiffies(wsa881x_ocp_poll_timer_sec * 1000));
 }
 
@@ -947,7 +947,7 @@ static int wsa881x_spkr_pa_event(struct snd_soc_dapm_widget *w,
 		break;
 	case SND_SOC_DAPM_POST_PMU:
 		wsa881x_spkr_pa_ctrl(component, true);
-		schedule_delayed_work(&wsa881x->ocp_ctl_work,
+ 		queue_delayed_work(system_power_efficient_wq, &wsa881x->ocp_ctl_work,
 			msecs_to_jiffies(WSA881X_OCP_CTL_TIMER_SEC * 1000));
 		break;
 	case SND_SOC_DAPM_PRE_PMD:
