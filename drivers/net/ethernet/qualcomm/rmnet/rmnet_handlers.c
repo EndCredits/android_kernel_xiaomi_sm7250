5595b6a688c9fc6cc16e4cf0f89ce3a2dc600142
EndCredits <alicization.han@gmail.com>
2021-05-02 10:23:11 +0800

    drivers: net: Switch to defined function

--- drivers/net/ethernet/qualcomm/rmnet/rmnet_handlers.c
+++ drivers/net/ethernet/qualcomm/rmnet/rmnet_handlers.c
@@ -445,7 +445,7 @@ rx_handler_result_t rmnet_rx_handler(struct sk_buff **pskb)
 	trace_rmnet_low(RMNET_MODULE, RMNET_RCV_FROM_PND, 0xDEF,
 			0xDEF, 0xDEF, 0xDEF, NULL, NULL);
 	dev = skb->dev;
-	port = rmnet_get_port_rcu(dev);
+	port = rmnet_get_port(dev);
 	if (unlikely(!port)) {
 		atomic_long_inc(&skb->dev->rx_nohandler);
 		kfree_skb(skb);
